cbd1459aa6ea5b9874b89d5db10240a69be6c06d
Calixto López Santos <calixto.lopez.santos@gmail.com>
2021-08-23 16:11:55 +0000

    Feat: text note

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [notes, setNotes] = useState([]);
+	const [myListNotes, setMyListNotes] = useState([]);
+
+	const addNote = note => {
+		if (event.key === "Enter") {
+			setNotes([...notes, note.target.value]);
+			event.target.value = "";
+		}
+	};
+
+	useEffect(() => {
+		setMyListNotes(
+			notes.map((element, index) => (
+				<li key={index.toString()}>{element}</li>
+			))
+		);
+	}, [notes]);
+
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<h1>Notas:</h1>
+
+			<input
+				type="text"
+				name="note"
+				id="note"
+				onKeyPress={event => {
+					addNote(event);
+				}}
+				placeholder="Escribir notas.."
+			/>
+
+			{myListNotes}
 		</div>
 	);
 };
---
+++ src/js/component/task.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Task = () => {
+
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+Greeting.propTypes = {
+  name: PropTypes.string
+};
+
+export default Task;
